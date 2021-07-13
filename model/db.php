85c4decbca8ce560e02fc282edf97895f26019c6
Viddeliten <johanna.julen@gmail.com>
2021-07-13 13:46:05 +0200

    Checking tables when connecting to database

--- model/db.php
+++ model/db.php
@@ -51,18 +51,25 @@ class db extends mysqli {
 					. mysqli_connect_error());
 		}
 		parent::set_charset('utf-8');
+        
+        // Check table structure
+        $this->checkTables();
 	}
 
 	/**
-	 * runs a query
+	 * runs a query and handles errors by throwing warnings
 	 **/
 	public function dbquery($query)
 	{
-		if($this->query($query))
-		{
-			return true;
+		try {
+            if(!$this->query($query))
+                trigger_error($this->error_list[0]['error'], E_USER_WARNING);
+            else
+                return true;
+        } catch ( Exception $e) {
+            trigger_error(sprinf("%s Code: %s", $e->getMessage(), $e->getCode()), E_USER_WARNING);
 		}
-
+        return false;
 	}
 
 	/**
@@ -89,17 +96,15 @@ class db extends mysqli {
          /* create a prepared statement */
         $stmt = $this->prepare($query);
 
+        // Statement prepare failed. Return false.
+        if($stmt === false)
+        {
+            return false;
+        }
+
         /* bind parameters for markers */
         if(!empty($param))
-        {
-            foreach($param as $p)
-            {
-                if(is_numeric($p))
-                    $stmt->bind_param("i", $p); // integer
-                else
-                    $stmt->bind_param("s", $p); // string
-            }
-        }
+            $this->bindParams($stmt, $param);
         
         /* execute query */
         $stmt->execute();
@@ -130,7 +135,6 @@ class db extends mysqli {
             $columns[]=$key;
         }
 
-        
         // Query
         $query = "INSERT INTO ".$table."
         (".implode(",", $columns).")
@@ -139,10 +143,21 @@ class db extends mysqli {
         /* create a prepared statement */
         $stmt = $this->prepare($query);
 
-        // Bind values
+        /* bind parameters for markers */
+        $this->bindParams($stmt, $values);
+        
+        /* execute query */
+        $stmt->execute();
+	}
+    
+    /**
+     * bind parameters for markers
+     **/
+    private function bindParams($statement, $params)
+    {
         $bind_vales = array();
         $bind_string="";
-        foreach($values as $key => $val)
+        foreach($params as $key => $val)
         {
             if(is_numeric($val))
                 $bind_string.="i"; // integer
@@ -151,11 +166,8 @@ class db extends mysqli {
             
             $bind_vales[]=$val;
         }
-        $stmt->bind_param($bind_string, ...$bind_vales);
-        
-        /* execute query */
-        $stmt->execute();
-	}
+        $statement->bind_param($bind_string, ...$bind_vales);
+    }
 
     /**
      * Updates values of an array into a table
@@ -200,6 +212,183 @@ class db extends mysqli {
         /* execute query */
         $stmt->execute();
 	}
+	
+    /**
+     * Checks table structure of database against the structure file in model catalogue and fixes any discrepancies (that are handled by this code)
+     **/
+	private function checkTables()
+	{
+		// Get all the table information
+		$structure_tables = $this->getTablesFromFile("model/structure.sql");
+        
+        if(!empty($structure_tables))
+        {
+            // For each table, check the structure
+            foreach($structure_tables as $table => $structure_fields)
+            {
+                // Get the current table if exists
+                $result = $this->fetch("SHOW CREATE TABLE ".$table);
+                if($result === false)
+                {
+                    if(stristr($this->error_list[0]['error'],"doesn't exist"))
+                    {
+                        // Table does not exist. Create it!
+                        echo '<p>'.sprintf(_("Table %s dows not exist. Creating..."), $table).'</p>'; // I know it's mispelled. I thought it was cute/funny :)
+                        $this->dbquery($structure_fields['create']);
+                        continue; // We just created the table so we don't need to check it
+                    }
+                }
+                            
+                $existing_fields = $this->getTableStructureFromCreateTable($result[0]['Create Table']);
+                
+                // Compare "columns", "extra_rows" and "last_row" between "structure_fields" and "existing_fields"
+                foreach($structure_fields['columns'] as $column_name => $contents)
+                {
+                    if(!isset($existing_fields['columns'][$column_name]))
+                    {
+                        // The column does not exist, add it
+                        echo '<p>'.sprintf(_("Adding column %s to table %s"),$column_name, $table).'</p>';
+                        $query = "ALTER TABLE ".$table." ADD `".$column_name."` ".$contents;
+                        if(!$this->dbquery($query))
+                            echo '<p class="error">Error 0858251</p>';
+                    }
+                    else if(strcmp($existing_fields['columns'][$column_name],$contents))
+                    {
+                        // The column exists, but is different
+                        echo '<p>'.sprintf(_("Modifying column %s in table %s"),$column_name, $table).'</p>';
+                        $query = "ALTER TABLE ".$table." MODIFY `".$column_name."` ".$contents;
+                        if(!$this->dbquery($query))
+                            echo '<p class="error">Error 0858260</p>';
+                    }
+                }
+                
+                // Compare extra rows (usually keys)
+                foreach($structure_fields['extra_rows'] as $key => $contents)
+                {
+                    if(!in_array($contents, $existing_fields['extra_rows']))
+                    {
+                        // If the row does not exist, modify table
+                        echo '<p>'.sprintf(_("Modifying table %s: %s"),$table, $contents).'</p>';
+                        $query = "ALTER TABLE ".$table." ".$contents;
+                        if(!$this->dbquery($query))
+                            echo '<p class="error">Error 0956274</p>';
+                    }
+                }
+                
+                /* Last row */
+                // Ignore the auto increment
+                $existing_last_row = preg_replace('/\sAUTO_INCREMENT=[^\s]*/',"",$existing_fields['last_row'][0]);
+                if(strcmp($existing_last_row,$structure_fields['last_row'][0]))
+                {
+                    // The last row is different
+                    echo '<p>'.sprintf(_("Modifyings table %s: %s"),$table, $structure_fields['last_row'][0]).'</p>';
+                    $query = "ALTER TABLE ".$table." ".$structure_fields['last_row'][0];
+                    if(!$this->dbquery($query))
+                        echo '<p class="error">Error 0958285</p>';
+                }
+                
+                // Alter tables
+                if(!empty($structure_fields['alter']))
+                {
+                    foreach($structure_fields['alter'] as $query)
+                    {
+                        // Run the alter, but suppress errors
+                        // (this is not handled very nicely, but it's not the end of the world if the foreign key restraints is not there at this point)
+                        @$this->dbquery($query);
+                    }
+                }
+            }
+        }
+	}
+	
+    /**
+     * Reads a sql file to find table structures
+     * @param   $filename string    Path to file to be read
+     * @returns array   structured array containing a member for each table
+     **/
+	private function getTablesFromFile($fileName)
+	{
+		// First we read the structure database file
+        if(file_exists($fileName))
+            $structure_sql = file_get_contents($fileName);
+        else
+        {
+            echo "<p>Found no structure</p>";
+            return null;
+        }
+            
+		// Now we want to find all the create table queries with regexp
+        $table_structure = array();
+        $tables_matched = array();
+        $pattern = '/CREATE TABLE[\sA-Za-z]*`([^`]*)`([^;]*)/';
+        if( preg_match_all($pattern, $structure_sql, $tables_matched))
+        {
+            foreach($tables_matched[1] as $key => $table)
+            {
+                // Get individual table structure
+                $table_structure[$table] = $this->getTableStructureFromCreateTable($tables_matched[0][$key]);
+            }
+        }
+        
+        // we also want the alter tables
+        $pattern = '/ALTER TABLE[\sA-Za-z]*`([^`]*)`\s*([^;]*)/';
+        if( preg_match_all($pattern, $structure_sql, $alters_matched))
+        {
+            foreach($alters_matched[1] as $key => $table)
+            {
+                // Get individual table structure
+                $table_structure[$table]['alter'][] = $alters_matched[0][$key];
+            }
+        }
+
+        return $table_structure;
+	}
+    
+    /**
+     * Creates an array of the table structure from a single create table statement. Only useful in the checkTables context
+     **/
+    private function getTableStructureFromCreateTable($string)
+    {
+        $table_structure = array();
+        
+        // For each table we want the columns
+        $columns_matched = array();
+        $pattern = '/\n\s*`(.*)`\s*([^,]*)/';
+        if( preg_match_all($pattern, $string, $columns_matched))
+        {
+            foreach($columns_matched[1] as $c_key => $column)
+            {
+                $table_structure['columns'][$column] = $columns_matched[2][$c_key];
+            }
+        }                    
+        
+        //Key rows start with capital letters and no `
+        $extra_rows_matched = array();
+        $pattern = '/\n\s*([A-Z][^,\n]*)/';
+        if( preg_match_all($pattern, $string, $extra_rows_matched))
+        {
+            foreach($extra_rows_matched[1] as $extra_row)
+            {
+                $table_structure['extra_rows'][] = $extra_row;
+            }
+        }                    
+
+        //Finding last row
+        $last_row_matched = array();
+        $pattern = '/[)]([^\n]*)$/';
+        if( preg_match_all($pattern, $string, $last_row_matched))
+        {
+            foreach($last_row_matched[1] as $last_row)
+            {
+                $table_structure['last_row'][] = $last_row;
+            }
+        }
+
+        // Also save the create table in the array
+        $table_structure['create'] = $string;
+        
+        return $table_structure;
+    }
 }
 
 ?>
